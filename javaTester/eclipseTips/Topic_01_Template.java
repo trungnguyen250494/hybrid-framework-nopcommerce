264b0c80a29bdd64df73edd045c19717f96c43ad
Trung Nguyen <trungnguyen250494@gmail.com>
2022-12-13 16:10:23 +0700

    Add Topic 40 - Eclipse tips

---
+++ javaTester/eclipseTips/Topic_01_Template.java
@@ -0,0 +1,18 @@
+package eclipseTips;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Topic_01_Template {
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		System.out.println();
+
+		WebElement field = driver.findElement(By.xpath(""));
+		
+		WebElement textBox = driver.findElement(By.cssSelector(""));
+	}
+
+}
---
+++ javaTester/eclipseTips/Topic_02_Debug.java
@@ -0,0 +1,29 @@
+package eclipseTips;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Topic_02_Debug {
+	String projectPath = System.getProperty("user.dir");
+	WebDriver driver;
+
+	@Test
+	public void TC_01() {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.facebook.com/");
+
+		String headerText = driver.findElement(By.xpath("//h2[contains(text(),\"Facebook helps\")]")).getText();
+		Assert.assertEquals(headerText, "Facebook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống của bạn.");
+
+		driver.quit();
+
+	}
+
+}
