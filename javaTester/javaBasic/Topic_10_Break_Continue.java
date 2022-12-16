f909e25e797260b6dfe99291e0de6c8c00259189
Trung Nguyen <trungnguyen250494@gmail.com>
2022-12-16 22:19:51 +0700

    Add Topic 42 - Java Basic - While/Array

---
+++ javaTester/javaBasic/Topic_10_Break_Continue.java
@@ -0,0 +1,19 @@
+package javaBasic;
+
+import java.util.ArrayList;
+
+public class Topic_10_Break_Continue {
+	public static void main(String[] args) {
+		
+		int[] studentNumbers = {12, 7, 6 ,1 ,2};
+		
+		for (int i = 0; i < studentNumbers.length; i++) {
+			System.out.println(studentNumbers[i]);
+		}
+		
+		ArrayList<String> stdName = new ArrayList<String>();
+		
+		
+	}
+
+}
