fab589d8279345a514579cd57e9dc5fd66087f66
Trung Nguyen <trungnguyen250494@gmail.com>
2022-12-29 17:17:58 +0700

    Add Topic 51 - Framework - Multiple Browsers

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,34 @@
 package commons;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+	private WebDriver driver;
+	private String projectPath = System.getProperty("user.dir");
+	private long timeout = 30;
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		if(browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+			driver = new FirefoxDriver();
+		}else if(browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+			driver = new ChromeDriver();
+		}else if(browserName.equals("edge")) {
+			System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver");
+			driver = new EdgeDriver();
+		}else {
+			throw new RuntimeException("Invalid browserName");
+		}
+		
+		driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://demo.nopcommerce.com/");
+		return driver;
+	}
 }
---
+++ testCases/com/nopcommerce/user/Level_04_Multiple_Browsers.java
@@ -0,0 +1,82 @@
+package com.nopcommerce.user;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import pageObjects.HomePageObject;
+import pageObjects.RegisterPageObject;
+
+public class Level_04_Multiple_Browsers extends BaseTest{
+	private WebDriver driver;
+	private String firstName,lastName,password,confirmPassword;
+	private HomePageObject homePage;
+	private RegisterPageObject registerPage;
+
+	@Parameters("browser")
+	@BeforeClass
+	public void beforeClass(String browserName) {
+		driver = getBrowserDriver(browserName);
+		homePage = new HomePageObject(driver);
+		
+		firstName = "Tester";
+		lastName = "Tester";
+		password = "Tester@123";
+		confirmPassword = "Tester@123";
+
+	}
+
+	@Test
+	public void Register_01_Register_Empty_Data() {
+		
+		System.out.println("Register_01 - Step 01: Click to the Register link");
+		
+		homePage.clickToRegisterLink();
+		
+		registerPage = new RegisterPageObject(driver);
+		
+		System.out.println("Register_01 - Step 02: Click to the Register button");
+
+		registerPage.clickToRegisterButton();
+
+		System.out.println("Register_01 - Step 03: Verify all error messages display");
+		Assert.assertEquals(registerPage.getErrorMessageAtFirstNameTextbox(), "First name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtLastNameTextbox(), "Last name is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Email is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtPasswordTextbox(), "Password is required.");
+		Assert.assertEquals(registerPage.getErrorMessageAtConfirmPasswordTextbox(), "Password is required.");
+		
+	}
+
+	@Test
+	public void Register_02_Register_Invalid_Email() {
+
+		System.out.println("Register_02 - Step 01: Click to the Register link");
+		homePage.clickToRegisterLink();
+		registerPage = new RegisterPageObject(driver);
+
+		System.out.println("Register_02 - Step 02: Input all fields value");
+		registerPage.inputToFirstNameTextbox(firstName);
+		registerPage.inputToLasttNameTextbox(lastName);
+		registerPage.inputToEmailTextbox("abc#xyz");
+		registerPage.inputToPasswordTextbox(password);
+		registerPage.inputToConfirmPasswordTextbox(confirmPassword);
+		
+		System.out.println("Register_02 - Step 03: Click to the Register button");
+		registerPage.clickToRegisterButton();	
+
+		System.out.println("Register_02 - Step 04: Verify wrong email - error message displays");
+		Assert.assertEquals(registerPage.getErrorMessageAtEmailTextbox(), "Wrong email");
+
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
