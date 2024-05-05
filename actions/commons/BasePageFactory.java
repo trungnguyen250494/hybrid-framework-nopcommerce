af632a2f81f7d9753d0d0e4f6bb93de8107455a8
Trung Nguyen Tri <trungtn@localhost>
2024-05-05 17:55:50 +0700

    Add Topic 19 - Allure Report

--- actions/commons/BasePageFactory.java
+++ actions/commons/BasePageFactory.java
@@ -2,7 +2,7 @@ package commons;
 
 import java.util.List;
 import java.util.Set;
-
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
--- actions/pageObjects/portal/UserHomePageObject.java
+++ actions/pageObjects/portal/UserHomePageObject.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 import commons.BasePage;
 import commons.PageGeneratorManager;
+import io.qameta.allure.Step;
 import pageUIs.user.HomePageUI;
 
 public class UserHomePageObject extends BasePage{
@@ -13,34 +14,40 @@ public class UserHomePageObject extends BasePage{
 		this.driver = driver;
 	}
 
+	@Step("Click to the Register link")
 	public UserRegisterPageObject clickToRegisterLink() {
 		waitForElementClickable(driver, HomePageUI.REGISTER_LINK);
 		clickToElement(driver, HomePageUI.REGISTER_LINK);
 		return PageGeneratorManager.getUserRegisterPage(driver);
 	}
 
+	@Step("Click to the Login link")
 	public UserLoginPageObject clickToLoginLink() {
 		waitForElementClickable(driver, HomePageUI.LOGIN_LINK);
 		clickToElement(driver, HomePageUI.LOGIN_LINK);
 		return PageGeneratorManager.getUserLoginPage(driver);
 	}
 
+	@Step("Verify if the Log out link is displayed")
 	public boolean isLogoutLinkDisplayed() {
 		waitUntilElementVisible(driver, HomePageUI.LOGOUT_LINK);
 		return isElementDisplayed(driver, HomePageUI.LOGOUT_LINK);
 	}
 
+	@Step("Verify if the My Account Link is displayed")
 	public boolean isMyAccountLinkDisplayed() {
 		waitUntilElementVisible(driver, HomePageUI.MY_ACCOUNT_LINK);
 		return isElementDisplayed(driver, HomePageUI.MY_ACCOUNT_LINK);
 	}
 
+	@Step("Click to the My Account link")
 	public UserCustomerPageObject clickToMyAccountLink() {
 		waitForElementClickable(driver, HomePageUI.MY_ACCOUNT_LINK);
 		clickToElement(driver, HomePageUI.MY_ACCOUNT_LINK);
 		return PageGeneratorManager.getUserCustomerInfoPage(driver);
 	}
-
+	
+	@Step("Verify if the Register link is displayed")
 	public boolean isRegisterLinkDisplayed() {
 		waitUntilElementVisible(driver, HomePageUI.REGISTER_LINK);
 		return isElementDisplayed(driver, HomePageUI.REGISTER_LINK);
