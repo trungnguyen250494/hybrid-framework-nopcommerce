a42a4d82ba975d6a354f510cc43a8ee574ecab0f
Trung Nguyen Tri <trungtn@localhost>
2024-06-05 17:03:10 +0700

    Update TC 101

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -17,7 +17,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
